aee8d8b17c4201b7882a8f7714e3ae971e98fc84
Nitin Mishra <76727343+imnitin28@users.noreply.github.com>
2022-12-19 12:11:30 +0530

    Update blockMaliciuosIP.sh

--- blockMaliciuosIP.sh
+++ blockMaliciuosIP.sh
@@ -2,3 +2,6 @@
 echo "Enter the malicious IP address to block"
 read ip_address
 route add "${ip_address}" reject
+
+# Check if entered IP is blocked.
+# route -n |grep <ip_address> # uncomment to check
